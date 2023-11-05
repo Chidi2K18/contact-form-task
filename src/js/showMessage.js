e7dca5ffcac0d2108133ab8a7e4920c7ebd6848b
nathanael mosaku <37617151+Chidi2K18@users.noreply.github.com>
2023-11-05 09:20:22 +0000

    Completed task

--- src/js/showMessage.js
+++ src/js/showMessage.js
@@ -1,8 +1,16 @@
-let messageContent = {
+export const messageContent = {
 	"success": "Your form has been successfully submitted!",
 	"error": "There was a validation error. Please check your input!",
 	"alreadySubmitted": "You have already submitted this form!",
+    "emailValidationFailed": "Your email is invalid. Please check your input and try again",
+    "messageEmpty":"Your message field is empty"
 };
+
+/**
+ * I've added more specific error messages 
+ * If email validation fails then an error message specific to emails is triggered
+ * If message validation fails then an error message speciic to messages is triggered
+ */
 
 let successState = false;
 
@@ -16,33 +24,38 @@ export function setHasSubmitted(value) {
     successState = value;
 }
 
-export function showSuccessMessage(didSucceed) {
+/**
+ * - `getHasSubmitted` was being called as a boolean instead of as a function, missing '()' 
+ *   so it was being treated as an object and non-null objects evaluate to true.
+ * - `setHasSubmitted(true)` (done in validate form) should be called on a successful submission before getting its state.
+ * - Additionally, the `showSuccessMessage` function should not require either the getter or setter functions 
+ *   as its purpose is to change output based on the state on submission.
+ */
+
+/**
+ * I've added an extra parameter to enable specific error messages passed from validate form on failed submission
+ * The corresponding error message would be passed and displayed
+ */
+export function showSuccessMessage(didSucceed, errorMessage = messageContent.error) { 
     const modal = document.getElementById('modal');
     const modalMessage = document.getElementById('modal-message');
     const closeModal = document.querySelector('.close-btn');
 
     if (didSucceed) {
-		// TODO: The `alreadySubmitted` message is wrongly showing before 
-		// the user has successfully submitted the form. We need to fix this!
-        if (getHasSubmitted) {
-            modalMessage.textContent = messageContent.alreadySubmitted;
-        } else {
-            modalMessage.textContent = messageContent.success;
-            setHasSubmitted(true);
-        }
+        modalMessage.textContent = messageContent.success;
     } else {
-        modalMessage.textContent = messageContent.error;
+        modalMessage.textContent = errorMessage;
     }
 
     modal.style.display = "block";
 
     closeModal.onclick = function() {
         modal.style.display = "none";
-    }
+    };
 
     window.onclick = function(event) {
         if (event.target === modal) {
             modal.style.display = "none";
         }
-    }
+    };
 }
--- src/js/validateForm.js
+++ src/js/validateForm.js
@@ -1,4 +1,4 @@
-import { showSuccessMessage, setHasSubmitted } from './showMessage.js';
+import { showSuccessMessage, setHasSubmitted, getHasSubmitted, messageContent } from './showMessage.js';
 
 export function validate() {
     const form = document.querySelector('form');
@@ -7,13 +7,42 @@ export function validate() {
 
         const email = document.querySelector('input[name="email"]').value;
         const message = document.querySelector('textarea[name="message"]').value;
+        
+        /**
+         * Checking for no whitespace before '@' 
+         * Checking for '@' symbol 
+         * Checking for '.' after the '@' 
+         * Checking for no whitespace after '@' 
+         */
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/; 
+        
+        let isValid = true;
+        let errorMessage = '';
 
-        // Basic form validation: check for '@' in email field and non-blank message field
-        // TODO: This validation could probably be improved!
-        if (email.includes('@') && message) {
+        //Check if the input matches with the regex pattern
+        if (!emailRegex.test(email)) {
+            isValid = false;
+            errorMessage = messageContent.emailValidationFailed; //display the specific message
+        }
+        //Check if message field is empty
+        else if (!message.trim()) {
+            isValid = false;
+            errorMessage = messageContent.messageEmpty; //display the specific message
+        }
+
+         // If the form has already been submitted successfully
+         if (getHasSubmitted()) {
+            showSuccessMessage(false, messageContent.alreadySubmitted); //the already submitted error message will trigger on subsequent submissions
+            return;
+        }
+
+        // If both email and message are valid and the form has not been submitted before
+        if (isValid) {
+            setHasSubmitted(true); // form has been submitted
             showSuccessMessage(true);
         } else {
-            showSuccessMessage(false);
+            showSuccessMessage(false, errorMessage); //return the specific error message
         }
+
     });
 }
